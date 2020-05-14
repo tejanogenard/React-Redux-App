6e8cfdb994504def9f03ba851a503a8a1e7cc81a
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-05-13 21:47:10 -0700

    simple styles added

--- balldontlie/src/components/PlayerCard.js
+++ balldontlie/src/components/PlayerCard.js
@@ -6,7 +6,6 @@ const PlayerCard = props => {
     return(
         <div className = "player-card">
         <p>Player: {props.player.first_name} {props.player.last_name}, {props.player.team.full_name}</p>
-
         </div>
     )
 }
